98c9b6ef69e0f09af94afa477bf40ef6d83e9f93
selbieh <selbieh@gmail.com>
2024-02-11 10:35:27 +0200

    add email creds

--- apps/ratings/views.py
+++ apps/ratings/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 
 from apps.ratings.models import Rating
 from apps.ratings.serializers import RatingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    # permission_classes = []
+    permission_classes = [IsAuthenticated]
